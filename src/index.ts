381bc39128d5141a0ba5986b418edc718fa5a825
mamaz <hisma.sudradjat@btpn.com>
2018-11-05 22:27:21 +0700

    Add debugging ability for Typescript

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export const hello = (string: string) => {
+  console.log("Hello ", string);
+};
+export const helloAgain = (string: string) => {
+  console.log("Hello Again", string);
+};
+hello('memezo');
+helloAgain('tafia');
